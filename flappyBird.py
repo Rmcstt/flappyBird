9ffdd4a0236afeb2644a9d621b11c6d5d7e0f0c3
Rmcstt <anisio.nanato@hotmail.com>
2024-04-07 19:43:52 -0300

    test

--- flappyBird.py
+++ flappyBird.py
@@ -1,46 +1,33 @@
 import pygame
 import os
 import random
+import neat
+
+ai_jpgando = True
+geracao = 0
+
 
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'pipe.png')))
-IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'base.png')))
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bg.png')))
+IMAGEM_CANO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('img', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('img', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('img', 'bg.png')))
 IMAGENS_PASSARO = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird3.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('img', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('img', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('img', 'bird3.png'))),
 ]
 
 pygame.font.init()
-FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+FONTE_PONTOS = pygame.font.SysFont('arial', 40)
 
-class Button:
-    def __init__(self, text, x, y, width, height, color, hover_color, action):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.hover_color = hover_color
-        self.text = text
-        self.action = action
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-        pygame.draw.rect(screen, (0, 0, 0), self.rect, 2)
-
-        font = pygame.font.SysFont('arial', 30)
-        text = font.render(self.text, True, (0, 0, 0))
-        text_rect = text.get_rect(center=self.rect.center)
-        screen.blit(text, text_rect)
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                self.action()
-
-def reload_game():
-    main()
 
 class Passaro:
     IMGS = IMAGENS_PASSARO
@@ -108,16 +95,18 @@ class Passaro:
 
         # desenhar a imagem
         imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
-        pos_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center
+        pos_centro_imagem = self.imagem.get_rect(
+            topleft=(self.x, self.y)).center
         retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
         tela.blit(imagem_rotacionada, retangulo.topleft)
 
     def get_mask(self):
         return pygame.mask.from_surface(self.imagem)
 
+
 class Cano:
-    DISTANCIA = 200
-    VELOCIDADE = 5
+    # DISTANCIA = random.randint(160, 200)
+    VELOCIDADE = 10
 
     def __init__(self, x):
         self.x = x
@@ -132,7 +121,7 @@ class Cano:
     def definir_altura(self):
         self.altura = random.randrange(50, 450)
         self.pos_topo = self.altura - self.CANO_TOPO.get_height()
-        self.pos_base = self.altura + self.DISTANCIA
+        self.pos_base = self.altura + random.randint(100, 200)
 
     def mover(self):
         self.x -= self.VELOCIDADE
@@ -157,8 +146,9 @@ class Cano:
         else:
             return False
 
+
 class Chao:
-    VELOCIDADE = 5
+    VELOCIDADE = 10
     LARGURA = IMAGEM_CHAO.get_width()
     IMAGEM = IMAGEM_CHAO
 
@@ -180,38 +170,8 @@ class Chao:
         tela.blit(self.IMAGEM, (self.x1, self.y))
         tela.blit(self.IMAGEM, (self.x2, self.y))
 
-class Button:
-    def __init__(self, text, x, y, width, height, color, hover_color, action=None):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
-        self.hover_color = hover_color
-        self.text = text
-        self.action = action
-    
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-        pygame.draw.rect(screen, (0, 0, 0), self.rect, 2)
 
-        font = pygame.font.SysFont('arial', 30)
-        text = font.render(self.text, True, (0, 0, 0))
-        text_rect = text.get_rect(center=self.rect.center)
-        screen.blit(text, text_rect)
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                if self.action:
-                    self.action()
-
-# Função para reiniciar o jogo
-def reiniciar_jogo():
-    global pontos, passaros, canos, chao
-    pontos = 0
-    passaros = [Passaro(230, 350)]
-    chao = Chao(730)
-    canos = [Cano(700)]
-
-def desenhar_tela(tela, passaros, canos, chao, pontos, botao_reload=None):
+def desenhar_tela(tela, passaros, canos, chao, pontos):
     tela.blit(IMAGEM_BACKGROUND, (0, 0))
     for passaro in passaros:
         passaro.desenhar(tela)
@@ -220,25 +180,39 @@ def desenhar_tela(tela, passaros, canos, chao, pontos, botao_reload=None):
 
     texto = FONTE_PONTOS.render(f"Pontuação: {pontos}", 1, (255, 255, 255))
     tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
+
+    if ai_jpgando:
+        texto = FONTE_PONTOS.render(f"Geração: {geracao}", 1, (255, 255, 255))
+        tela.blit(texto, (10, 10))
+
     chao.desenhar(tela)
-    if botao_reload:
-        botao_reload.draw(tela)
     pygame.display.update()
 
-def main():
-    passaros = [Passaro(230, 350)]
+
+def main(genomas, config):
+    global geracao
+    geracao += 1
+
+    if ai_jpgando:
+        redes = []
+        lista_genomas = []
+        passaros = []
+        for _, genoma in genomas:
+            rede = neat.nn.FeedForwardNetwork.create(genoma, config)
+            redes.append(rede)
+            genoma.fitness = 0
+            lista_genomas.append(genoma)
+            passaros.append(Passaro(230, 350))
+    else:
+        passaros = [Passaro(230, 350)]
     chao = Chao(730)
     canos = [Cano(700)]
     tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
     pontos = 0
     relogio = pygame.time.Clock()
 
-    # Botão de Reload
-    
-
     rodando = True
     while rodando:
-        botao_reload = None
         relogio.tick(30)
 
         # interação com o usuário
@@ -246,18 +220,29 @@ def main():
             if evento.type == pygame.QUIT:
                 rodando = False
                 pygame.quit()
-                botao_reload = Button("Reload", 200, 400, 100, 50, (0, 255, 0), (0, 200, 0), reload_game)
                 quit()
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_SPACE:
-                    for passaro in passaros:
-                        passaro.pular()
-            if botao_reload:
-                botao_reload.handle_event(evento)
-
+            if not ai_jpgando:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_SPACE:
+                        for passaro in passaros:
+                            passaro.pular()
+        indice_cano = 0
+        if len(passaros) > 0:
+            if len(canos) > 1 and passaros[0].x > (canos[0].x + canos[0].CANO_TOPO.get_width()):
+                indice_cano = 1
+        else:
+            rodando = False
+            break
         # mover as coisas
-        for passaro in passaros:
+        for i, passaro in enumerate(passaros):
             passaro.mover()
+            lista_genomas[i].fitness += 0.1
+            output = redes[i].activate((passaro.y,
+                                        abs(passaro.y -
+                                            canos[indice_cano].altura),
+                                        abs(passaro.y - canos[indice_cano].pos_base)))
+            if output[0] > 0.5:
+                passaro.pular()
         chao.mover()
 
         adicionar_cano = False
@@ -266,6 +251,10 @@ def main():
             for i, passaro in enumerate(passaros):
                 if cano.colidir(passaro):
                     passaros.pop(i)
+                    if ai_jpgando:
+                        lista_genomas[i].fitness -= 1
+                        lista_genomas.pop(i)
+                        redes.pop(i)
                 if not cano.passou and passaro.x > cano.x:
                     cano.passou = True
                     adicionar_cano = True
@@ -276,14 +265,40 @@ def main():
         if adicionar_cano:
             pontos += 1
             canos.append(Cano(600))
+            for genoma in lista_genomas:
+                genoma.fitness += 5
         for cano in remover_canos:
             canos.remove(cano)
 
         for i, passaro in enumerate(passaros):
             if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
                 passaros.pop(i)
+                if ai_jpgando:
+                    lista_genomas.pop(i)
+                    redes.pop(i)
 
-        desenhar_tela(tela, passaros, canos, chao, pontos, botao_reload)
+        desenhar_tela(tela, passaros, canos, chao, pontos)
+
+
+def rodar(caminho_config):
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                caminho_config)
+
+    populacao = neat.Population(config)
+
+    populacao.add_reporter(neat.StdOutReporter(True))
+    populacao.add_reporter(neat.StatisticsReporter())
+
+    if ai_jpgando:
+        populacao.run(main, 50)
+    else:
+        main(None, None)
+
 
 if __name__ == '__main__':
-    main()
+    caminho = os.path.dirname(__file__)
+    caminho_config = os.path.join(caminho, 'config.txt')
+    rodar(caminho_config)
